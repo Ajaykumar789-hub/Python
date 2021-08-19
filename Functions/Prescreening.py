39de24a4fd6e36e0673bd1b09434f564021502e4
Ajay kumar <ajaykumar.sqlbi@gmail.com>
2021-08-19 16:09:46 -0400

    adding prescreening questions to github

---
+++ Functions/Prescreening.py
@@ -0,0 +1,18 @@
+#mylist=[0,1,2,3,4,5,6,7,8]
+#print(mylist[-6:-1])
+
+#myname='John Doe'
+
+#print(myname.split(' ')[1])
+"""
+for i in range(3):
+     if i==1: continue
+print(i) """
+
+mydict={'a':1,'e':5}
+print(mydict.keys())
+
+a = [1, 2, 3]
+
+a = a[::-1]
+print(a)
