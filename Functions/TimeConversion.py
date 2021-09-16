cbcf6d8f41e2ffbdbb15bac587c45403c30210f8
Ajay kumar <ajaykumar.sqlbi@gmail.com>
2021-09-15 20:09:20 -0400

    adding time conversion function to git

---
+++ Functions/TimeConversion.py
@@ -0,0 +1,22 @@
+
+
+
+
+a=''
+def timeConversion(s):
+   if s[-2:] == "AM" :
+      if s[:2] == '12':
+          a = str('00' + s[2:8])
+      else:
+          a = s[:-2]
+   else:
+      if s[:2] == '12':
+          a = s[:-2]
+      else:
+          a = str(int(s[:2]) + 12) + s[2:8]
+   return a
+
+
+s = '03:44:14PM'
+result = timeConversion(s)
+print(result)
